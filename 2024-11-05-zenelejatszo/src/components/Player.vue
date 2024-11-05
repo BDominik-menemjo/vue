490f7d3c3885a95545bc61229952b531edb20682
BDominik-menemjo <dominikbelai@gmail.com>
2024-11-05 09:59:40 +0100

    csekerés

--- 2024-11-05-zenelejatszo/src/components/Player.vue
+++ 2024-11-05-zenelejatszo/src/components/Player.vue
@@ -2,11 +2,13 @@
     import AudioPlayer from 'vue3-audio-player';
     import 'vue3-audio-player/dist/style.css';
     import Track from '../classes/Track.js';
-    import { computed, defineProps } from 'vue';
+    import { computed, defineProps, ref } from 'vue';
 
     const props=defineProps({
         track: Track
     });
+
+    const audioRef=ref(null);
 
     const trackInfo = computed(()=>
         props.track ? props.track.getTitle() +"-"+props.track.getArtist() : "Nincs zeneszám kiválasztva!"
@@ -21,12 +23,28 @@
         src: props.track ? getAudioURL(props.track.getFile()) : '',
         title: props.track ? props.track.getTitle() : "Nincs zeneszám"
     }));
+
+    const start=()=>{
+        if(props.track && audioRef.value){
+            audioRef.value.play();
+        }else{
+            alert("Nincs kiválasztott zeneszám!");
+        }
+    };
+
+    const stop=()=>{
+        if(props.track && audioRef.value){
+            audioRef.value.pause();
+        }
+    };
 </script>
 
 <template>
     <section>
         <h2>Jelenlegi szám: <em>{{ trackInfo }}</em></h2>
-        <AudioPlayer :option="audioOptions"/>
+        <AudioPlayer ref="audioRef" :option="audioOptions"/>
+        <button @click="start()">Lejátszás</button>
+        <button @click="stop()">Megállitás</button>
     </section>
     
 </template>
@@ -36,6 +54,14 @@
         text-align: center;
     }
     h2{
-        font-size: 20px;
+        font-size: 18px;
+    }
+    button{
+        padding: 10px 15px;
+        margin: 5px;
+        color: #fff;
+        background-color: #007bff;
+        border: none;
+        border-radius: 5px;
     }
 </style>
