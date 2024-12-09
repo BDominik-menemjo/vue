05548e75afa3fbea1c0b8fabe5535ddcb3103781
BDominik-menemjo <dominikbelai@gmail.com>
2024-12-09 11:26:48 +0100

    dolgozat commit

---
+++ 2024-11-26-dolgozat/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Books from './components/Books.vue'
+import Cart from './components/Cart.vue'
+</script>
+
+<template>
+  <Books />
+  <Cart />
+</template>
+
+<style scoped>
+  
+</style>
---
+++ 2024-11-26-dolgozat/src/components/Books.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+    <main>
+        <section class="felso">
+            <p>Keresés a könyvek között</p>
+            <p>Kölcsönzött: db</p>
+            <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                <label class="form-check-label" for="flexSwitchCheckDefault">Minden könyv mutatása</label>
+            </div>
+            <button class="btn btn-warning">Tovább</button>
+        </section>
+        <section>
+            <p>Találatok: db</p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-4 konyv">
+                     <img src="../assets/book1.jpg" alt="Piknik" title="piknik">
+                        <p>Piknik</p>
+                        <p>STA-001</p>
+                        <button class="btn btn-primary">Kölcsönzés</button>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 konyv">
+                        <img src="../assets/book1.jpg" alt="Piknik" title="piknik">
+                        <p>Piknik</p>
+                        <p>STA-001</p>
+                        <button class="btn btn-primary">Kölcsönzés</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<style scoped>
+    .konyv{
+        border: 1px solid #fff;
+        display: block;
+        text-align: center;
+        padding: 10px;
+    }
+    
+</style>
