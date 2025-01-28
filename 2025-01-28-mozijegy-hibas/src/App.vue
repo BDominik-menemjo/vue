3a2dcf7d32fb7da2f5657a6f032213ec299748ef
BDominik-menemjo <dominikbelai@gmail.com>
2025-01-28 12:40:48 +0100

    kisebb javítások, de még mindig hibás

--- 2025-01-28-mozijegy-hibas/src/App.vue
+++ 2025-01-28-mozijegy-hibas/src/App.vue
@@ -1,19 +1,18 @@
 <script setup>
-import Screeing from "./classes/Screening.js"
+import Screening from "./classes/Screening.js"
 import { roomsData } from './data/rooms.js';
 import { movieDatas } from './data/movies.js';
 import { screenings } from './state/state.js';
-import { RouterView } from 'vue-router';
 import { screeningsData } from "./data/screenings.js";
 
 const movieState=screenings();
 
 //State feltöltése
 if(movieState.getScreeningsLength()==0){
-  for(let i=0; i<screeningsData.lenght; i++){
-    const seats = roomsData.find(room=>room.id===screeningsData[i]);
+  for(var i=0; i<screeningsData.lenght; i++){
+    const seats = roomsData.find(room=>room.id===screeningsData[i].roomId);
     const seatsCopy= seats.seats.map(row=>[...row]);
-    var s= new Screening(screeningsData[i].movieId, screeningsData[i].roomId, screeningsData[i].time, seatsCopy);
+    var s= new Screening(screeningsData[i].movieId, screeningsData[i].roomId, screeningsData[i].time, seatsCopy)
     movieState.addMovie(s);
   }
 }
